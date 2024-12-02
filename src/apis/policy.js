768314707da5323f293e900a717c97245d087518
김연진 <kyj0719@gmail.com>
2024-12-02 21:33:40 +0900

    feat: 북마크 api 연결, baseURL 설정, 파싱함수 utils로 분리

--- src/apis/policy.js
+++ src/apis/policy.js
@@ -2,21 +2,24 @@ import axios from 'axios';
 
 const getSinglePolicy = async (srchPolicyId) => {
   const { data } = await axios.get(
-    `https://api.youthstepup.site/policy/recommend/${srchPolicyId}`
+    `${import.meta.env.VITE_API_BASE_URL}/policy/recommend/${srchPolicyId}`
   );
   return data;
 };
 
 const getRecommendPolicy = async (page) => {
   const { data } = await axios.get(
-    `https://api.youthstepup.site/policy/recommend?display=10&pageIndex=${page}`
+    `${import.meta.env.VITE_API_BASE_URL}/policy/recommend?display=10&pageIndex=${page}`,
+    {
+      withCredentials: true,
+    }
   );
   return data;
 };
 
 const getRandomPolicy = async (page) => {
   const { data } = await axios.get(
-    `https://api.youthstepup.site/policy/recommend/random?display=10&pageIndex=${page}`
+    `${import.meta.env.VITE_API_BASE_URL}/policy/recommend/random?display=10&pageIndex=${page}`
   );
   return data;
 };
