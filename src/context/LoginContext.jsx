5c353f47654db1d91e8be5d04d783e16994aa275
Whatdoyumin <lym4963@naver.com>
2024-12-07 01:52:27 +0900

    feat: 전역 상태 관리에 닉네임, 프로필 이미지 추가

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -3,20 +3,40 @@ import { createContext, ReactNode, useState, useEffect } from 'react';
 export const LoginContext = createContext({
   isLogin: false,
   setIsLogin: () => {},
+  nickName: '',
+  setNickName: () => {},
+  profileImgUrl: '',
+  setProfileImgUrl: () => {},
+  kakaoProfileImg: '',
+  setKakaoProfileImg: () => {},
 });
 
 export function LoginContextProvider({ children }) {
   const [isLogin, setIsLogin] = useState(false);
+  const [nickName, setNickName] = useState('');
+  const [profileImgUrl, setProfileImgUrl] = useState('');
+  const [kakaoProfileImg, setKakaoProfileImg] = useState('');
 
   useEffect(() => {
-    const accessToken = document.cookie
-      .split('; ')
-      .find((row) => row.startsWith('accessToken='));
-    setIsLogin(accessToken ? true : false);
-  }, []);
+    console.log('isLogin: ', isLogin);
+    console.log('nickName: ', nickName);
+    console.log('profileImgUrl: ', profileImgUrl);
+    console.log('kakaoProfileImg: ', kakaoProfileImg);
+  }, [isLogin, nickName, profileImgUrl, kakaoProfileImg]);
 
   return (
-    <LoginContext.Provider value={{ setIsLogin, isLogin }}>
+    <LoginContext.Provider
+      value={{
+        setIsLogin,
+        isLogin,
+        nickName,
+        setNickName,
+        profileImgUrl,
+        setProfileImgUrl,
+        kakaoProfileImg,
+        setKakaoProfileImg,
+      }}
+    >
       {children}
     </LoginContext.Provider>
   );
