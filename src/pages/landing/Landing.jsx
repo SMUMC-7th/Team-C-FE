c2e1c11a2e5f4e199651a4157a12565d0fb0cb55
김연진 <kyj0719@gmail.com>
2024-12-16 18:37:21 +0900

    feat: 알림 권한 요청 기능 추가

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -3,9 +3,21 @@ import { RiKakaoTalkFill } from 'react-icons/ri';
 import { useNavigate } from 'react-router-dom';
 import logo_wg from '../../images/logo_wg.svg';
 
+function requestPermission() {
+  console.log('권한 요청 중...');
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('알림 권한이 허용됨');
+
+      // FCM 메세지 처리
+    } else {
+      console.log('알림 권한 허용 안됨');
+    }
+  });
+}
 function Landing() {
   const loginUrl = `${import.meta.env.VITE_KAKAO_URL}?response_type=code&client_id=${import.meta.env.VITE_KAKAO_REST_API_KEY}&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}`;
-
+  requestPermission();
   return (
     <S.Container>
       <S.LogoContainer>
