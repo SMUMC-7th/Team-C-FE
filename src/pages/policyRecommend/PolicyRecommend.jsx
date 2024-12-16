e4ad67b38368ad0ceb7167c2b1d0a99f680630f0
김연진 <kyj0719@gmail.com>
2024-12-16 11:58:32 +0900

    feat: navigate으로 변경

--- src/pages/policyRecommend/PolicyRecommend.jsx
+++ src/pages/policyRecommend/PolicyRecommend.jsx
@@ -1,27 +1,53 @@
 import * as S from './PolicyRecommend.style';
 import PolicyList from '../../components/policyList/policyList';
-import { useState } from 'react';
-import userInfo from '../../mocks/userData.json';
 import { updateVh } from '../../utils/calculateVH';
-
-const user = userInfo[0];
+import { useContext, useState, useEffect } from 'react';
+import { LoginContext } from '../../context/LoginContext';
+import Portal from '../../components/Portal';
+import ContentModal from '../../components/modal/ContentModal';
+import { Navigate } from 'react-router-dom';
 
 function PolicyRec() {
-  const [isLogin, setIsLogin] = useState(true);
-  updateVh();
-  window.addEventListener('resize', updateVh);
+  const { isLogin } = useContext(LoginContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = Navigate();
+  useEffect(() => {
+    updateVh();
+    const handleResize = () => updateVh();
+    window.addEventListener('resize', handleResize);
 
-  if (isLogin === false) {
-    alert('로그인이 필요한 서비스입니다');
-    window.location.href = '/home';
-  }
+    if (isLogin === false) {
+      setIsModalOpen(true);
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isLogin]);
 
   return (
-    <S.Container>
-      <S.PolicyContainer>
-        <PolicyList isLogin={isLogin} {...user}></PolicyList>
-      </S.PolicyContainer>
-    </S.Container>
+    <>
+      <S.Container>
+        <S.PolicyContainer>
+          <PolicyList></PolicyList>
+        </S.PolicyContainer>
+      </S.Container>
+      {isModalOpen && (
+        <Portal>
+          <ContentModal
+            title="로그인하시겠습니까?"
+            message="로그인이 필요한 서비스입니다."
+            btnText1="로그인"
+            btnText2="닫기"
+            onBtn1Click={() => navigate('/')}
+            onBtn2Click={() => {
+              navigate('/home');
+              setIsModalOpen(false);
+            }}
+          />
+        </Portal>
+      )}
+    </>
   );
 }
 
